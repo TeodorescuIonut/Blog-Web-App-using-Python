a99f597e800ce7351eb911a0858150763caace28
TeodorescuIonut <teodorescuionutz@gmail.com>
2022-04-12 12:24:12 +0300

    added image preview and view

--- static/app.js
+++ static/app.js
@@ -11,10 +11,16 @@ file.onchange = function(e) {
     case 'png':
     case 'tif':
     case 'jpeg':
-      alert('Allowed');
       break;
     default:
-      alert('Not allowed');
+      alert('Not allowed! Please add a valid type - jpg,bmp,png,tif,jpeg');
       this.value = '';
-  }
 };
+if(event.target.files.length > 0){
+    var src = URL.createObjectURL(event.target.files[0]);
+    console.log(src)
+    var preview = document.getElementById("file-ip-1-preview");
+    preview.src = src;
+    preview.style.display = "block";
+  };
+  };
